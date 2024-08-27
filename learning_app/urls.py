a4910d147b3b1e356d37bf47c5b28429558f09c9
BrunoEzekiel <eps.bruno@outlook.com>
2024-08-26 22:07:20 -0300

    add-new-templates-cadastro-newtopic

--- learning_app/urls.py
+++ learning_app/urls.py
@@ -6,5 +6,6 @@ app_name = 'learning_app'
 urlpatterns = [
     path('', views.index, name='index'),
     path('topics/', views.topics, name='topics'),
-
+    path('topic/<int:topic_id>/', views.topic, name='topic'),
+    path('new_topic/', views.new_topic, name='new_topic'),
 ]
--- learning_app/views.py
+++ learning_app/views.py
@@ -1,12 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from.forms import TopicForm
 from .models import Topic
-# Create your views here.
-def index(request):#pagina inicial do site
+
+def index(request):
     return render(request, 'index.html')
-    
 
 def topics(request):
-    topics = Topic.objects.order_by('date_added')
-    context = {'topics' : topics}
+    topics = Topic.objects.all()
+    context = {'topics': topics}
     return render(request, 'topics.html', context)
 
+def topic(request, topic_id):
+    topic = get_object_or_404(Topic, id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'topic.html', context)
+
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_app:topics'))
+    context = {'form': form}
+    return render(request, 'new_topic.html', context)
